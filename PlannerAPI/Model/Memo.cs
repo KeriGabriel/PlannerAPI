6d6320a21f1190205cbcc66afb075466d57dc53c
Keri <79213053+KeriGabriel@users.noreply.github.com>
2022-05-14 10:25:36 -0700

    removed nullable enabled

--- PlannerAPI/Model/Memo.cs
+++ PlannerAPI/Model/Memo.cs
@@ -2,10 +2,10 @@
 {
     public class Memo
     {
-        public string? MemoId { get; set; }
-        public string? Text { get; set; }
+        public string MemoId { get; set; }
+        public string Text { get; set; }
         public DateTime Date { get; set; }
-        public string? Type { get; set; }
-        public string? Description { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
     }
 }
